43673bdcaac01bd6feaad8a8e7ff2d41a6c4b106
jcbjcb <782661403@qq.com>
2019-06-06 11:19:57 +0800

    增加消息类型

--- rabbitMq/src/main/java/com/example/demo/config/RabbitConfig.java
+++ rabbitMq/src/main/java/com/example/demo/config/RabbitConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,9 +12,53 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
 
+    //queue
 
     @Bean
     public Queue helloQueue() {
         return  new Queue("hello");
     }
+
+    @Bean
+    public Queue topicMessage() {
+        return new Queue("topic.message");
+    }
+
+    @Bean
+    public Queue fanoutQueue() {
+        return new Queue("fanout.A");
+    }
+
+
+    //exchange
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("exchange");
+    }
+    @Bean
+    FanoutExchange fanoutExchange() {
+        return new FanoutExchange("fanoutExchange");
+    }
+
+    //bind
+
+    //绑定单个topicExchange
+    @Bean
+    Binding bindingExchangeMessage(Queue topicMessage, TopicExchange exchange) {
+        return BindingBuilder.bind(topicMessage).to(exchange).with("topic.message");
+    }
+
+    //绑定多个topicExchange
+    @Bean
+    Binding bindingExchangeMessages(Queue topicMessage, TopicExchange exchange) {
+        return BindingBuilder.bind(topicMessage).to(exchange).with("topic.#");
+    }
+
+    //绑定单个 fanoutExchange
+    @Bean
+    Binding bindingFanoutExchange(Queue fanoutQueue ,FanoutExchange fanoutExchange) {
+        return BindingBuilder.bind(fanoutQueue).to(fanoutExchange);
+    }
+
 }
