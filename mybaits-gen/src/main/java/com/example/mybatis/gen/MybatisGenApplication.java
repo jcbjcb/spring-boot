0ffe7f3640b823bd9be1393305a62ff9ed05e666
jcbjcb <782661403@qq.com>
2019-06-11 13:50:46 +0800

    mybatis 代码自动生成工具

---
+++ mybaits-gen/src/main/java/com/example/mybatis/gen/MybatisGenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatis.gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisGenApplication.class, args);
+    }
+
+}
